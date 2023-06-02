204982b703406b5ab60af9c4ed1fe46888aa70ac
Koding Kidd <105122189+kodingfinesse@users.noreply.github.com>
2023-06-02 12:28:06 -0700

    two files
    two files

--- conky/conky_start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sleep 5
-/usr/bin/conky -q -c ~/.config/conky/sysinfo_conkyrc
-#/usr/bin/conky -q -c ~/.config/conky/kernlog_conkyrc
-#/usr/bin/conky -q -c ~/.config/conky/gkeep_conkyrc
-/usr/bin/conky -q -c ~/.config/conky/aliases_conkyrc
-/usr/bin/conky -q -c ~/.config/conky/meteo_conkyrc
